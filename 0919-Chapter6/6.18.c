46043abac57f78c74c78dd1dee9a5e2800ded68a
William <2298536003@qq.com>
2019-09-20 22:04:30 +0800

    date0919-question6.18,6.20 added

---
+++ 0919-Chapter6/6.18.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    printf ("First Format:\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4d", i);
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4c", '-');
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++)
+    {
+        for (int j=1 ; j<=9 ; j++) {
+            printf ("%-4d", i*j);
+        }
+        printf ("\n");
+    }
+    
+    printf ("\nSecond Format:\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4d", i);
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4c", '-');
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++)
+    {
+        for (int j=1 ; j<=9 ; j++) {
+            if(i>=j) printf ("%-4d", i*j);
+            else printf ("%-4c", ' ');
+        }
+        printf ("\n");
+    }
+    
+    printf ("\nThird Format:\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4d", i);
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++) printf ("%-4c", '-');
+    printf ("\n");
+    
+    for (int i=1 ; i<=9 ; i++)
+    {
+        for (int j=1 ; j<=9 ; j++) {
+            if(i<=j) printf ("%-4d", i*j);
+            else printf ("%-4c", ' ');
+        }
+        printf ("\n");
+    }
+}
---
+++ 0919-Chapter6/6.20.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+    for (int i=0 ; (i*i)<=99999 ; i++) {
+        number = i*i;
+        int digit[5] = {0};
+        for (int j=0 ; j<=4 ; j++) {
+            digit[j] = number % 10;
+            number /= 10;
+        }
+        if (digit[0]==digit[1] && digit[3]==digit[4]) {
+//            if(i*i<10) printf("0000");
+//            else if(i*i<100) printf("000");
+//            else if (i*i<1000) printf("00");
+//            else if (i*i<10000) printf("0");
+            // commented code above can also fill '0' before numbers <= 10000
+            
+            int k=0;
+            for (k=4 ; k>=1 ; k--)
+                if (digit[k] != 0) break;
+            for (; k!=4 ; k++)
+                printf ("0");
+            
+            printf ("%d\n", i*i);
+        }
+    }
+}
